0b472ad8f6ae30ce884540ac3b3714310facfe5e
colourhand <673553877@qq.com>
2015-12-30 10:39:04 +0800

    整理plugin三个组件

--- plugin/提示对话框/js.js
+++ plugin/提示对话框/js.js
@@ -1,3 +1,26 @@
 /**
  * Created by colourhand on 15/12/29.
  */
+function test(){
+    Tips('我会在两秒后自动关闭哦',2000);
+}
+function Tips(msg, duration){
+    var insetHtml='<div id="sha-back"><div class="sha-wrapper"><div class="sha-tips-content">'+msg+'</div></div></div>';
+    var temp=document.createElement('div');
+    temp.id="shaTemp";
+    temp.innerHTML=insetHtml;
+    var last=document.body.lastChild;
+    document.body.insertBefore(temp,last);
+    if(typeof duration=='number'&& duration>0){
+        setTimeout(function(){
+            var temp=document.getElementById('shaTemp');
+            temp.parentNode.removeChild(temp);
+        },duration);
+    }
+    else {
+        setTimeout(function(){
+            var temp=document.getElementById('shaTemp');
+            temp.parentNode.removeChild(temp);
+        },1000);
+    }
+}
--- plugin/确认取消对话框/js.js
+++ plugin/确认取消对话框/js.js
@@ -1,3 +1,52 @@
 /**
  * Created by colourhand on 15/12/29.
  */
+function test(){
+    shaDialog('友情提醒','本活动归彩色手工作室所有',1);
+}
+//type  0:默认:确定  1:确定 and  取消
+function shaDialog(title,content,type){
+    var dialog ={
+        title:title,
+        content:content,
+        type:type
+    };
+    var insetHtml=
+        "<div id='sha-dialog'>"+
+        "<div id='sha-title'>"+dialog.title+"</div>"+
+        "<div id='sha-content'>"+dialog.content+"</div>"+
+        "<div id='sha-buttons'>";
+    if(type==1){
+        insetHtml+=  "<button onclick='cancel()'>取消</button><button id='sure' onclick='sure()'>确定</button>"+
+            "</div> </div>";
+    }
+    else{
+        insetHtml+=  "<button id='onlySure' onclick='sure()'>确定</button>"+
+            "</div> </div>";
+    }
+    var temp=document.createElement('div');
+    temp.id="sha-temp";
+    temp.innerHTML=insetHtml;
+    var first=document.body.lastChild;
+    document.body.insertBefore(temp,first);
+    document.getElementById('sha-dialog').style.display='block';
+}
+
+function cancel(){
+    console.log('取消');
+    document.getElementById('sha-dialog').style.display='none';
+    document.body.className='';
+    removeDialogDiv('sha-temp');
+    return false;
+}
+function sure() {
+    console.log('确定');
+    document.getElementById('sha-dialog').style.display = 'none';
+    document.body.className='';
+    removeDialogDiv('sha-temp');
+    return true;
+}
+function removeDialogDiv(id){
+    var temp=document.getElementById(id);
+    temp.parentNode.removeChild(temp);
+}
